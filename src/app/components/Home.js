9eeb5a943b794dd2b93adbb3bbe08240c8e40199
Ryan Henry <ryan.henry20@hotmail.com>
2017-08-10 02:58:02 +0700

    State of Components

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,13 +1,30 @@
 import React from "react";
 
 export class Home extends React.Component{
+  constructor(props){
+    super();
+    this.state ={
+        age: props.initialAge,
+        status: 0
+    };
+  }
+
+  onMakeOlder(){
+    this.setState({
+        age: this.state.age +3
+    });
+  }
+
   render(){
     var text = "Something";
     return (
         <div>
           <p>In a new Component!</p>
           <p>{text}</p>
-          <p>Your name is {this.props.name}, your age is {this.props.age}</p>
+          <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+          <p>Status : {this.state.status}</p>
+          <hr/>
+          <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make Me older</button>
         </div>
     );
   }
@@ -15,5 +32,5 @@ export class Home extends React.Component{
 
 Home.propTypes = {
   name: React.PropTypes.string,
-    age: React.PropTypes.number
+    initialAge: React.PropTypes.number
 };
--- src/app/index.js
+++ src/app/index.js
@@ -5,7 +5,7 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component{
-    render(){ 
+    render(){
       return (
           <div className="container">
             <div className="row">
@@ -15,7 +15,7 @@ class App extends React.Component{
             </div>
             <div className="row">
               <div className="col-xs-10 col-xs-offset-1">
-                <Home name={"Ryan"} age={24} />
+                <Home name={"Ryan"} initialAge={24} />
               </div>
             </div>
           </div>
